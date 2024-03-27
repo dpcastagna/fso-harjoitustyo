727444d365c1cef506e80d2dbef34560bbbaa903
Daniele Castagna <daniele.castagna@outlook.com>
2024-03-27 12:06:07 +0200

    dayjs käyttöön ShiftsMonthissa, kuukausi taulukko aluillaan

--- src/components/ShiftsMonth.jsx
+++ src/components/ShiftsMonth.jsx
@@ -1,19 +1,73 @@
-
+import '../App.css'
+import { useState } from "react"
+import dayjs from "dayjs"
+import localeData from "dayjs/plugin/localeData"
+// import weekday from "dayjs/plugin/weekday"
+dayjs.extend(localeData)
+// dayjs.extend(weekday)
+// import fi from "dayjs/locale/fi"
+// dayjs.locale('fi')
 
 const ShiftsMonth = (props) => {
+  const [month, setMonth] = useState(0)
+  const [year, setYear] = useState(2024)
+  const months = dayjs.months()
+  const years = Array.from({ length: 11 }, (_, i) => /* new Date().getFullYear() */ 2020 + i)
+  const days = dayjs(`${year}-${month}-15`).daysInMonth()
 
 
-  const daysOfMonth = (y, m) => {
-    return new Date(y, m, 0).getDate()
+  // const daysOfMonth = (y, m) => {
+  //   return new Date(y, m, 0).getDate()
+  // }
+
+  const handleMonthChange = (event) => {
+    setMonth(event.target.value)
+  }
+  const handleYearChange = (event) => {
+    setYear(event.target.value)
   }
 
-  console.log(daysOfMonth(2024, 2))
-
+  console.log(month, year, days, years, dayjs(`${year}-${month}-15`))
+  
   return (
-    <>
-      2024 Mar {daysOfMonth(2024, 3)} <br/>
-      2024 Feb {daysOfMonth(2024, 2)}
-    </>
+    <div id="palikka">
+      <select 
+        name="months"
+        id="monthSelect"
+        defaultValue={months[dayjs().month()]}
+        onChange={handleMonthChange}
+      >
+        {
+          months.map(month => {
+            return (
+              <option key={month} value={month} >{month}</option>
+            )
+          })
+        }
+      </select>
+      <select 
+        name="years"
+        id="yearSelect"
+        defaultValue={dayjs().year()}
+        onChange={handleYearChange}
+      >
+        {
+          years.map(year => {
+            return (
+              <option key={year} value={year} >{year}</option>
+            )
+          })
+        }
+      </select>
+      <table id="monthTable">
+        <th>Employee</th>
+        {
+          <tr>
+            <td>{days}</td>
+          </tr>
+        }
+      </table>
+    </div>
   )
 }
 
